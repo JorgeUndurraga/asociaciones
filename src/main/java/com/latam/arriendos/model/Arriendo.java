342f6042186d3d6a9fccc198f4b7f0088fe1e59e
JorgeUndurraga <jorgeundurraga@yahoo.com>
2021-02-28 03:54:00 -0300

    Capa Modelos: Arriendo y Película

---
+++ src/main/java/com/latam/arriendos/model/Arriendo.java
@@ -0,0 +1,34 @@
+package com.latam.arriendos.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(name="SQ_ARRIENDO", initialValue=1, allocationSize=1)
+public class Arriendo {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.SEQUENCE,
+	generator="SQ_ARRIENDO")
+	private Integer id;
+	private String inicio;
+	private Integer duracion;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pelicula_id", referencedColumnName = "id")
+	private Pelicula pelicula;
+
+}
