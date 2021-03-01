a7a7040d66e1d05b235b099eb8a06a8a66fcf239
JorgeUndurraga <jorgeundurraga@yahoo.com>
2021-02-28 23:32:09 -0300

    Prueba unitaria Correcta
    Esta prueba unitaria arrojó buenos datos (a diferencia de la anterior). El pom es el correcto, porque funcionó posteriormente en la prueba luego de crear al Cliente.

--- src/test/java/com/latam/arriendos/AsociacionesApplicationTests.java
+++ src/test/java/com/latam/arriendos/AsociacionesApplicationTests.java
@@ -1,48 +1,59 @@
 package com.latam.arriendos;
 
-
+import com.latam.arriendos.model.Arriendo;
+import com.latam.arriendos.model.Cliente;
+import com.latam.arriendos.model.Pelicula;
+import com.latam.arriendos.repository.ArriendoRepository;
+import com.latam.arriendos.repository.ClienteRepository;
+import com.latam.arriendos.repository.PeliculaRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.latam.arriendos.model.Arriendo;
-import com.latam.arriendos.model.Pelicula;
-import com.latam.arriendos.repository.ArriendoRepository;
-import com.latam.arriendos.repository.PeliculaRepository;
-
+import java.util.*;
 
 @SpringBootTest
 class AsociacionesApplicationTests {
-	
+
 	@Autowired
 	private ArriendoRepository arriendoRepository;
-	
+
 	@Autowired
 	PeliculaRepository peliculaRepository;
 
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Test
-	private void validaRepositoriosCreandoPeliculaYArriendo() {
-		//nueva película
-		Pelicula spaceOdyssey = new Pelicula(null, "2001: Odisea del Espacio",
-				
-				"Después de descubrir un misterioso artefacto enterrado bajo la superficie lunar, la humanidad emprende una búsqueda para encontrar sus orígenes con la ayuda de la supercomputadora inteligente H.A.L. 9000",
-				
-				149);
-		
-		//nuevo arriendo
-		Arriendo arriendo = new Arriendo(null, "11/20/2019", 7, spaceOdyssey);
-		
-		//guardar arriendo
-		arriendoRepository.save(arriendo);
-		
-		//buscar arriendo
-		Arriendo arriendoOd = arriendoRepository.findById(1).get();
-		
-		//valida que exista
-		Assertions.assertNotNull(arriendoOd);
-		
+	public void validandoDatos() {
+		// Instanciamos las tres clases.
+		Pelicula pelicula = new Pelicula();
+		Cliente cliente = new Cliente();
+		Arriendo arriendo = new Arriendo();
+		// Guardamos al cliente en base de datos para generar su ID.
+		cliente.setNombre("Juan");
+		cliente = clienteRepository.save(cliente);
+		// Asignamos un estado a la pelicula
+		pelicula.setDescripcion("Buena pelicula");
+		pelicula.setDuracion(160);
+		pelicula.setTitulo("El cuadro");
+		// Asignamos un estado al arriendo
+		arriendo.setDuracion(10);
+		arriendo.setInicio("11/12/2019");
+		arriendo.setPelicula(pelicula);
+		// Le asignamos al cliente previamente creado.
+		arriendo.setCliente(cliente);
+		// Guardamos la pelicula y el arriendo
+		arriendo = arriendoRepository.save(arriendo);
+		// Creamos una lista de arriendos y agregamos el que ya creamos a ella.
+		List<Arriendo> arriendos = new ArrayList<>();
+		arriendos.add(arriendo);
+		cliente.setArriendos(arriendos);
+		// Guardamos al cliente en base de datos
+		cliente = clienteRepository.save(cliente);
+		// afirmaciones
+		Assertions.assertNotNull(arriendoRepository.findAll());
+		Assertions.assertNotNull(clienteRepository.findAll());
 	}
 
 }
